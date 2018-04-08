c94ad58f62bd89de8fbbc79d71b4df8d3c7ae604
johnmolin91 <john.molin91@gmail.com>
2018-04-08 08:54:51 -0400

    added build

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const router = require("express").Router();
+
+app.use(express.static("client/build"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, function() {
